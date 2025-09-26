3f78d18e967a9d11d368d6f87835690fbce57229
MarcSuarez <marcsua02@gmail.com>
2025-09-25 20:47:20 -0500

    Agregar archivos de proyecto.

---
+++ Cajero/Clases/Usuario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Cajero.Clases
+{
+    internal class Usuario
+    {
+        public string NumeroCuenta { get; set; }
+
+        public decimal Saldo { get; set; }
+        public int pin {  get; set; }
+
+        public void Depositar(decimal Cantidad)
+        {
+            if (Cantidad <= 0)
+            {
+                Console.WriteLine("El saldo a depositar debe de ser mayor que $0 ");
+                
+            }
+            else
+            {
+                Saldo += Cantidad;
+                Console.WriteLine($"La cantidad depositada es de {Cantidad}");
+                Console.WriteLine($"Su nuevo saldo es {Saldo}");
+                
+            }
+        }
+
+        public void Retirar(decimal Valor)
+        {
+            if (Saldo < Valor)
+            {
+                Console.WriteLine("El saldo isuficiente  ");
+
+            }
+            else
+            {
+                Saldo -= Valor;
+                Console.WriteLine($"La cantidad retirada es de {Valor}");
+                Console.WriteLine($"Su nuevo saldo es {Saldo}");
+
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Cajero/Program.cs
@@ -0,0 +1,42 @@
+﻿using Cajero.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cajero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario();
+            Console.WriteLine("Digite el monto de la cuenta");
+            usuario.Saldo = decimal.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Digite el monto a depositar ");
+            decimal Cantidad = decimal.Parse(Console.ReadLine());
+            usuario.Depositar(Cantidad);
+            Console.WriteLine(usuario.Saldo);
+
+            Console.WriteLine("Digite la Cantidad a retirar");
+            decimal valor = decimal.Parse(Console.ReadLine());
+            Console.WriteLine();
+            usuario.Retirar(valor);
+            Console.WriteLine();
+            
+
+
+
+
+
+            Console.ReadKey(true);
+
+            
+            
+
+        }
+    }
+}
