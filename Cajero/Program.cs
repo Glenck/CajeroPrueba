b8b34928b767f44bd8b3ef229a6d2e8acc0f068c
Glenck <231789606+Glenck@users.noreply.github.com>
2025-09-28 18:56:39 -0500

    actualizacion del codigo en busca de almacenar los usuarios

--- Cajero/Program.cs
+++ Cajero/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Cajero.Clases;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -12,30 +13,116 @@ namespace Cajero
     {
         static void Main(string[] args)
         {
-            Usuario usuario = new Usuario();
-            Console.WriteLine("Digite el monto de la cuenta");
-            usuario.Saldo = decimal.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine("Digite el monto a depositar ");
-            decimal Cantidad = decimal.Parse(Console.ReadLine());
-            usuario.Depositar(Cantidad);
-            Console.WriteLine(usuario.Saldo);
+            Usuario usuario = new Usuario(); // Instancia de la clase Usuario
+            string rutaUsuarios = ("C:\\Users\\Usuario\\Documents\\Git\\Cajero\\CajeroPrueba\\Archivos txt\\Usuarios.txt");
+            string rutaMovimientos = ("C:\\Users\\Usuario\\Documents\\Git\\Cajero\\CajeroPrueba\\Archivos txt\\Movimientos.txt");
+            //defino la ruta del archivo de texto donde se almacenan los usuarios y movimientos
 
-            Console.WriteLine("Digite la Cantidad a retirar");
-            decimal valor = decimal.Parse(Console.ReadLine());
-            Console.WriteLine();
-            usuario.Retirar(valor);
-            Console.WriteLine();
+            List<Usuario> listaUsuarios = new List<Usuario>()// Lista de usuarios predefinidos
+            {
+                new Usuario(){ NumeroCuenta = "1003687336", Nombre = "Fabio Andres", pin = 2701, Saldo = 10000000 },
+                new Usuario(){ NumeroCuenta = "1234567890", Nombre = "Daniel Hernandez", pin = 0987, Saldo = 2000000 },
+                new Usuario(){ NumeroCuenta = "1233492717", Nombre = "Aswin Niño", pin = 4562, Saldo = 15000000 },
+            };
+
+            using (StreamWriter tx = new StreamWriter(rutaUsuarios))
+            {
+                foreach (Usuario i in listaUsuarios)
+                {
+                    tx.WriteLine($"Cuenta:{usuario.NumeroCuenta}");
+                    tx.WriteLine($"Nombre: {usuario.Nombre}");
+                    tx.WriteLine($"Pin: {usuario.pin}");
+                    tx.WriteLine($"Saldo: {usuario.Saldo}");
+                    tx.WriteLine("------------------------");
+                }
+            }
             
 
 
 
 
 
-            Console.ReadKey(true);
 
-            
-            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            bool contrlador = true ;
+            while (contrlador)
+            {
+                Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                Console.WriteLine(":: Bienvenido al cajero automatico ::");
+                Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                Console.WriteLine("::::::  Seleccione una opcion  ::::::");
+                Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+
+                /*
+                Console.WriteLine(":: 1. Ingresar                     ::");
+                Console.WriteLine(":: 2. Salir                        ::");
+                
+                Console.WriteLine(":: 1. Depositar                    ::");
+                Console.WriteLine(":: 2. Retirar                      ::");
+                Console.WriteLine(":: 3. Consultar saldo              ::");
+                Console.WriteLine(":: 4. Movimientos                  ::");
+                Console.WriteLine(":: 5. Salir                        ::");
+
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion) {
+                    case 1:
+                        usuario.Depositar();
+                        break;
+
+                        case 2:
+                        usuario.Retirar();
+                        break;
+                        case 3:
+                      usuario.VerSaldo();
+                        break;
+                        case 5:
+                        contrlador = false;
+                        break;
+
+
+
+
+                }
+   
+                       */
+        }
 
         }
     }
