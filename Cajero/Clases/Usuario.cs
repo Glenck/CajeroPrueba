750d3fffc06673693ff1afc181d8a69fc2a05593
Glenck <231789606+Glenck@users.noreply.github.com>
2025-10-04 19:01:02 -0500

    actulizacion

--- Cajero/Clases/Usuario.cs
+++ Cajero/Clases/Usuario.cs
@@ -14,15 +14,22 @@ namespace Cajero.Clases
 
         public decimal Saldo { get; set; }
 
-        public int Pin { get; set; }
+        public string Pin { get; set; }
 
-    
 
+        public Usuario()
+        {
+            this.NumeroCuenta = NumeroCuenta;
+            this.Nombre = Nombre;
+            this.Saldo = Saldo;
+            this.Pin = Pin;
+        }
 
         public void Depositar()
         {
             Console.WriteLine("Digite el monto a depositar ");
             decimal Cantidad = decimal.Parse(Console.ReadLine());
+            Usuario.registroMovimientos($"Se depositaron = {Cantidad},{NumeroCuenta}");
 
             if (Cantidad <= 0)
             {
@@ -40,6 +47,7 @@ namespace Cajero.Clases
         {
             Console.WriteLine("Digite la Cantidad a retirar");
             decimal Valor = decimal.Parse(Console.ReadLine());
+            Usuario.registroMovimientos($"Se retiraron = {Valor},{NumeroCuenta}");
             if (Saldo < Valor)
             {
                 Console.WriteLine("El saldo isuficiente  ");
@@ -53,19 +61,96 @@ namespace Cajero.Clases
             Console.ReadKey(true);
         }
         public void VerSaldo()
+
         {
             Console.WriteLine($"Saldo actual: {Saldo}");
 
             Console.ReadKey(true);
+        }
+
+
+        public static bool IniciarSecion(string rutaUsuarios, string ComprobarUsuario, string ComprobarPin)
+        {
+            if (File.Exists(rutaUsuarios))
+            {
+                string[] lineas = File.ReadAllLines(rutaUsuarios);
+                foreach (string linea in lineas)
+                {
+                    string[] datos = linea.Split(',');
+
+                    if (datos[0] == ComprobarUsuario && datos[1] == ComprobarPin)
+                    {
+                        return true; // Credenciales válidas
+                    }
+                }
+            }
+
+            return false; // Credenciales inválidas o archivo no encontrado
+
 
         }
+        public void CargarUsuarios(string rutaUsuarios, string comprobarUsuario, string comprobarPin)
+        {
+
+
+            if (File.Exists(rutaUsuarios))
+            {
+                string[] lineas = File.ReadAllLines(rutaUsuarios);
+                foreach (string linea in lineas)
+                {
+                    string[] datos = linea.Split(',');
+                    if (datos.Length == 4)
+                    {
+
+                        string NumeroCuenta = datos[0];
+                        string Pin = datos[1];
+                        string Nombre = datos[2];
+                        decimal Saldo = decimal.Parse(datos[3]);
+                        if (comprobarUsuario == NumeroCuenta && comprobarPin == Pin)
+                        {
+
+                        }
+
+                    }
+                }
+            }
+
+        }
+        public static void registroMovimientos(string mensaje)
+        {
+            string rutaMovimientos = ("C:\\Users\\Usuario\\Documents\\Git\\CajeroPrueba\\Archivos txt\\Movimientos txt");
+
+            string texto = $"{DateTime.Now}, {mensaje},";
+
+            File.AppendAllText(rutaMovimientos, texto + Environment.NewLine);
+
+        }
+
+        public static List<string> MostrarMovimiento(string numerocuenta)
+        {
+            List<string> movimientos = new List<string>();
+            string rutaMovimientos = ("C:\\Users\\Usuario\\Documents\\Git\\CajeroPrueba\\Archivos txt\\Movimientos txt");
+            if (File.Exists(rutaMovimientos))
+            {
+                string[] linea = File.ReadAllLines(rutaMovimientos);
+                foreach (string s in linea)
+                {
+                    if (s.Contains($"{numerocuenta}"))
+                    {
+                        movimientos.Add(s);
+                        Console.WriteLine(linea[s]);
+                    }
+
+                }
+            }
+
+            return movimientos;
+
+        }
+
+
     }
 
 
 
-
-
-
-
 }
-
--- Cajero/Program.cs
+++ Cajero/Program.cs
@@ -13,213 +13,96 @@ namespace Cajero
     {
         static void Main(string[] args)
         {
-             // Instancia de la clase Usuario
-            string rutaUsuarios = ("C:\\Users\\Usuario\\Documents\\Git\\Cajero\\CajeroPrueba\\Archivos txt\\Usuarios.txt");
-            string rutaMovimientos = ("C:\\Users\\Usuario\\Documents\\Git\\Cajero\\CajeroPrueba\\Archivos txt\\Movimientos.txt");
+
+            // Instancia de la clase Usuario
+            string rutaUsuarios = ("C:\\Users\\Usuario\\Documents\\Git\\CajeroPrueba\\Archivos txt\\Usuarios.txt");
+            string rutaMovimientos = ("C:\\Users\\Usuario\\Documents\\Git\\CajeroPrueba\\Archivos txt\\Movimientos txt");
+            Usuario usuario = new Usuario();
+
+            //List<Usuario> ListaUsurios = CargarUsuarios(rutaUsuarios);
+
             //defino la ruta del archivo de texto donde se almacenan los usuarios y movimientos
 
 
 
-            Usuario usuario = new Usuario() { NumeroCuenta = "1003687336", Nombre = "Fabio Andres", Pin = 2701, Saldo = 10000000 };
-                
+            Console.WriteLine("Digite su usuario");
+            string ComprobarUsuario = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Digite su pin");
+            string ComprobarPin = Console.ReadLine();
 
-            
-            if (!File.Exists(rutaUsuarios))
+            bool accesoConcedido = Usuario.IniciarSecion(rutaUsuarios, ComprobarUsuario, ComprobarPin);
+
+            usuario.CargarUsuarios(rutaUsuarios, ComprobarUsuario, ComprobarPin);
+
+
+            if (accesoConcedido)
+
             {
-                using (StreamWriter tx = File.CreateText(rutaUsuarios))
+                //List<Usuario> listaUsuarios = CargarUsuarios(rutaUsuarios);
+
+                bool controlador = true;
+                while (controlador)
                 {
-
-                    tx.WriteLine($"Cuenta:{usuario.NumeroCuenta}");
-                    tx.WriteLine($"Nombre: {usuario.Nombre}");
-                    tx.WriteLine($"Pin: {usuario.Pin}");
-                    tx.WriteLine($"Saldo: {usuario.Saldo}");
-                    tx.WriteLine("------------------------");
-
-                }
-                Console.WriteLine("Archivo creado y texto escrito correctamente.");
-
-            }
-            else 
-            {
-                using (StreamWriter tx = File.AppendText(rutaUsuarios))
-                {
-
-                    tx.WriteLine($"Cuenta:{usuario.NumeroCuenta}");
-                    tx.WriteLine($"Nombre: {usuario.Nombre}");
-                    tx.WriteLine($"Pin: {usuario.Pin}");
-                    tx.WriteLine($"Saldo: {usuario.Saldo}");
-                    tx.WriteLine("------------------------");
-                   
-                }
-                Console.WriteLine("El archivo ya existe. Se ha agregado nuevo usuario.");
-            }
-                
+                    Console.Clear();
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine(":: Bienvenido al cajero automatico ::");
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine("::::::  Seleccione una opcion  ::::::");
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine(":: 1. Depositar                    ::");
+                    Console.WriteLine(":: 2. Retirar                      ::");
+                    Console.WriteLine(":: 3. Consultar saldo              ::");
+                    Console.WriteLine(":: 4. Movimientos                  ::");
+                    Console.WriteLine(":: 5. Salir                        ::");
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                    Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
 
 
+                    int opcion = int.Parse(Console.ReadLine());
 
+                    switch (opcion)
+                    {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*
-            bool contrlador = true ;
-            while (contrlador)
-            {
-                Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
-                Console.WriteLine(":: Bienvenido al cajero automatico ::");
-                Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
-                Console.WriteLine("::::::  Seleccione una opcion  ::::::");
-                Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
-
-                /*
-                Console.WriteLine(":: 1. Ingresar                     ::");
-                Console.WriteLine(":: 2. Salir                        ::");
-
-                Console.WriteLine(":: 1. Depositar                    ::");
-                Console.WriteLine(":: 2. Retirar                      ::");
-                Console.WriteLine(":: 3. Consultar saldo              ::");
-                Console.WriteLine(":: 4. Movimientos                  ::");
-                Console.WriteLine(":: 5. Salir                        ::");
-
-                int opcion = int.Parse(Console.ReadLine());
-
-                switch (opcion) {
-                    case 1:
-                        usuario.Depositar();
-                        break;
+                        case 1:
+                            usuario.Depositar();
+                            break;
 
                         case 2:
-                        usuario.Retirar();
-                        break;
+                            usuario.Retirar();
+                            break;
                         case 3:
-                      usuario.VerSaldo();
-                        break;
+                            usuario.VerSaldo();
+                            break;
+                        case 4:
+                            List<string> usuarios = Usuario.MostrarMovimiento(usuario.NumeroCuenta);
+                            break;
+
                         case 5:
-                        contrlador = false;
-                        break;
+                            controlador = false;
+                            break;
 
 
 
 
+                    }
                 }
 
-                       */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*
-            bool contrlador = true ;
-            while (contrlador)
+            }
+            else
             {
-                Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
-                Console.WriteLine(":: Bienvenido al cajero automatico ::");
-                Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
-                Console.WriteLine("::::::  Seleccione una opcion  ::::::");
-                Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                Console.Clear();
+                Console.WriteLine("Acceso denegado. Usuario o PIN incorrecto.");
+                accesoConcedido = false; // Salir del programa si el acceso es denegado
+            }
 
-                /*
-                Console.WriteLine(":: 1. Ingresar                     ::");
-                Console.WriteLine(":: 2. Salir                        ::");
-
-                Console.WriteLine(":: 1. Depositar                    ::");
-                Console.WriteLine(":: 2. Retirar                      ::");
-                Console.WriteLine(":: 3. Consultar saldo              ::");
-                Console.WriteLine(":: 4. Movimientos                  ::");
-                Console.WriteLine(":: 5. Salir                        ::");
-
-                int opcion = int.Parse(Console.ReadLine());
-
-                switch (opcion) {
-                    case 1:
-                        usuario.Depositar();
-                        break;
-
-                        case 2:
-                        usuario.Retirar();
-                        break;
-                        case 3:
-                      usuario.VerSaldo();
-                        break;
-                        case 5:
-                        contrlador = false;
-                        break;
-
-
-
-
-                }
-
-                       */
+            Console.ReadKey(true);
 
         }
 
-        
+
+
     }
+
+
 }
