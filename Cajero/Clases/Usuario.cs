90b3b86f4a86810093bf8591b0e0f2817c8652c1
Glenck <231789606+Glenck@users.noreply.github.com>
2025-10-04 23:16:21 -0500

    actualizacion final del cajero

--- Cajero/Clases/Usuario.cs
+++ Cajero/Clases/Usuario.cs
@@ -16,7 +16,8 @@ namespace Cajero.Clases
 
         public decimal Saldo { get; set; }
 
-        public string Pin { get; set; }
+        public string Pin { get; set; } //se definen las propiedades de la clase usuario
+
 
 
         public Usuario() 
@@ -25,19 +26,19 @@ namespace Cajero.Clases
             this.Nombre = Nombre;
             this.Saldo = Saldo;
             this.Pin = Pin;
-        }
+        }//constructor por defecto de la clase usuario
 
-        public void Depositar()
+        public void Depositar() // se define el metodo depositar
         {
             Console.WriteLine("Digite el monto a depositar ");
-            decimal Cantidad = decimal.Parse(Console.ReadLine());
-            Usuario.registroMovimientos($"Se depositaron = {Cantidad},{NumeroCuenta}");
+            decimal Cantidad = decimal.Parse(Console.ReadLine()); // se solicita y se lee la cantidad a depositar
+            Usuario.registroMovimientos($"Se depositaron = {Cantidad},{NumeroCuenta}"); // registra el movimiento en el archivo de texto
 
-            if (Cantidad <= 0)
+            if (Cantidad <= 0) // se valida que la cantidad a depositar sea mayor a 0
             {
                 Console.WriteLine("El saldo a depositar debe de ser mayor que $0 ");
             }
-            else
+            else // si la cantidad es valida se realiza el deposito
             {
                 Saldo += Cantidad;
                 Console.WriteLine($"La cantidad depositada es de {Cantidad}");
@@ -45,114 +46,149 @@ namespace Cajero.Clases
             }
             Console.ReadKey(true);
         }
-        public void Retirar()
+        public void Retirar() // se define el metodo retirar
         {
             Console.WriteLine("Digite la Cantidad a retirar");
-            decimal Valor = decimal.Parse(Console.ReadLine());
-            Usuario.registroMovimientos($"Se retiraron = {Valor},{NumeroCuenta}");
-            if (Saldo < Valor)
+            decimal Valor = decimal.Parse(Console.ReadLine()); // se solicita y se lee la cantidad a retirar
+            Usuario.registroMovimientos($"Se retiraron = {Valor},{NumeroCuenta}");// registra el movimiento en el archivo de texto
+
+            if (Saldo < Valor) // se valida que el saldo sea mayor a la cantidad a retirar
             {
-                Console.WriteLine("El saldo isuficiente  ");
+                Console.WriteLine("El saldo insuficiente  ");
             }
-            else
+            else // si la cantidad es valida se realiza el retiro
             {
                 Saldo -= Valor;
                 Console.WriteLine($"La cantidad retirada es de {Valor}");
-                Console.WriteLine($"Su nuevo saldo es {Saldo}");
+                Console.WriteLine($"Su nuevo saldo es {Saldo}"); // se muestra el nuevo saldo
             }
             Console.ReadKey(true);
         }
-        public void VerSaldo()
+        public void VerSaldo() // se define el metodo ver saldo
 
         {
-            Console.WriteLine($"Saldo actual: {Saldo}");
+            Console.WriteLine($"Saldo actual: {Saldo}"); // se muestra el saldo actual
 
             Console.ReadKey(true);
         }
 
-
-        /*  public static bool IniciarSecion(string rutaUsuarios, string ComprobarUsuario, string ComprobarPin)
-         {
-             if (File.Exists(rutaUsuarios))
-             {
-                 string[] lineas = File.ReadAllLines(rutaUsuarios);
-                 foreach (string linea in lineas)
-                 {
-                     string[] datos = linea.Split(',');                    
-
-                         if (datos[0] == ComprobarUsuario && datos[1] == ComprobarPin)
-                         {
-                             return true; // Credenciales válidas
-                         }                   
-                 }
-             }
-
-                 return false; // Credenciales inválidas o archivo no encontrado
-
-
-         } */
-        static Usuario iSA(string rutaUsuarios, string comprobarUsuario, string comprobarPin)
+        public static Usuario iSA(string rutaUsuarios, string comprobarUsuario, string comprobarPin) // metodo para comprobar si el usuario y pin son correctos
         {
-            if (File.Exists(rutaUsuarios))
+            if (File.Exists(rutaUsuarios)) // se valida que el archivo de texto exista
             {
-                foreach (string linea in File.ReadAllLines(rutaUsuarios))
+                foreach (string linea in File.ReadAllLines(rutaUsuarios)) // se lee el archivo de texto linea por linea
                 {
                     string ln = linea.Trim();
                     string[] datos = linea.Split(',');
-                    if (datos.Length < 4)
+                    if (datos.Length < 4) // se valida que la linea tenga los datos necesarios
                     {
                         continue;
                     }
-                    string numeroCuenta = datos[0].Trim();
+                    string numeroCuenta = datos[0].Trim(); // se asignan los datos a las variables
                     string pin = datos[1].Trim();
                     string nombre = datos[2].Trim();
                     decimal saldo = 0;
                     decimal.TryParse(datos[3].Trim(), out saldo);
-                    if (numeroCuenta == comprobarUsuario && pin == comprobarPin)
+                    if (numeroCuenta == comprobarUsuario && pin == comprobarPin) // se valida que el usuario y pin sean correctos
                     {
-                        return new Usuario{ NumeroCuenta = numeroCuenta,Pin = pin,Nombre = nombre,Saldo = saldo};
+                        return new Usuario{ NumeroCuenta = numeroCuenta,Pin = pin,Nombre = nombre,Saldo = saldo}; 
+                        // si son correctos se retorna un objeto de la clase usuario
                     }
                 }
             }
             return null; // Credenciales inválidas o archivo no encontrado  
         }
 
-        public static void registroMovimientos(string mensaje)
+        public static void registroMovimientos(string mensaje) // metodo para registrar los movimientos en un archivo de texto
         {
             string rutaMovimientos = ("C:\\Users\\Usuario\\Documents\\Git\\CajeroPrueba\\Archivos txt\\Movimientos txt");
+            string texto = $"{DateTime.Now}, {mensaje},"; // se crea el texto a registrar con la fecha y hora actual
 
-            string texto = $"{DateTime.Now}, {mensaje},";
+            File.AppendAllText(rutaMovimientos, texto + Environment.NewLine); // se escribe el texto en el archivo de texto
 
-            File.AppendAllText(rutaMovimientos, texto + Environment.NewLine);
-            
         }
 
-        public static List<string> MostrarMovimiento(string numerocuenta)
+        public void MostrarMovimiento(string numerocuenta) // metodo para mostrar los ultimos 5 movimientos del usuario
         {
-            List<string>movimientos = new List<string>();
             string rutaMovimientos = ("C:\\Users\\Usuario\\Documents\\Git\\CajeroPrueba\\Archivos txt\\Movimientos txt");
-            if (File.Exists(rutaMovimientos))
+           
+            if (!File.Exists(rutaMovimientos)) // se valida que el archivo de texto exista
             {
-                string[] linea = File.ReadAllLines(rutaMovimientos);
-                foreach (string s in linea)
-                {
-                    int i = 0;i++;
-                    if (s.Contains($"{numerocuenta}"))
-                    {
-                        movimientos.Add(s);
-                        Console.WriteLine(linea[i]);
-                    }
-                    
-                }
-             }
+                Console.WriteLine("No hay movimientos registrados.");
+                return;
+            }
 
-            return movimientos;
+            string[] lineas = File.ReadAllLines(rutaMovimientos); // se lee el archivo de texto linea por linea
+
+            List<string> movimientosCuenta = new List<string>(); // lista para almacenar los movimientos del usuario
+
+            foreach (string linea in lineas) // se recorre la lista de lineas
+            {
+                if (linea.Contains(numerocuenta)) // se valida que la linea contenga el numero de cuenta del usuario
+                {
+                    movimientosCuenta.Add(linea); // si es asi se agrega a la lista de movimientos del usuario
+                }
+            }
+           if(movimientosCuenta.Count == 0) // se valida que la lista de movimientos del usuario no este vacia
+            {
+                Console.WriteLine($"No hay movimientos registrados para la cuenta ");
+                return;
+            } 
+
+            var movimientosRecientes = movimientosCuenta.Skip(Math.Max(0,movimientosCuenta.Count -5)); // se obtienen los ultimos 5 movimientos del usuario
+
+            Console.WriteLine("Ultimos 5 movimientos:");
+            foreach (string movimiento in movimientosRecientes) // se recorre la lista de movimientos recientes
+            {
+                Console.WriteLine(movimiento); // se muestra el movimiento
+            }
+
 
         }
-        
+        public void CambiarPin() // metodo para cambiar el pin del usuario
+        {
+            string rutaUsuarios = ("C:\\Users\\Usuario\\Documents\\Git\\CajeroPrueba\\Archivos txt\\Usuarios.txt");
+            if (File.Exists(rutaUsuarios)) // se valida que el archivo de texto exista
+            {
+                string[] lineas = File.ReadAllLines(rutaUsuarios); // se lee el archivo de texto linea por linea
+                for (int i = 0; i < lineas.Length; i++) // se recorre la lista de lineas
+                {
+                    string[] datos = lineas[i].Split(','); // se separan los datos de la linea
+                    if (datos.Length < 4) // se valida que la linea tenga los datos necesarios
+                    {
+                        continue;
+                    }
+                    string cuenta = datos[0].Trim(); // se asignan los datos a las variables
+                    if (cuenta == NumeroCuenta) // se valida que el numero de cuenta sea igual al del usuario
+                    {
+                        Console.WriteLine("Digite su nuevo pin");
+                        string nuevoPin = Console.ReadLine().Trim(); // se solicita y se lee el nuevo pin
+                        int validor = int.Parse(nuevoPin);
+                        if (validor > 0) // se valida que el nuevo pin sea mayor a 0000
+                        {
+                            datos[1] = nuevoPin;
+                            lineas[i] = string.Join(",", datos); // se actualiza la linea con el nuevo pin
+                            File.WriteAllLines(rutaUsuarios, lineas);
+                            this.Pin = nuevoPin; // Actualizar el PIN en la instancia actual
+                            Console.WriteLine("Pin cambiado con exito");
+                            return;
+                        }
+                        else // si el nuevo pin no es valido se muestra un mensaje de error
+                        {
+                            Console.WriteLine("El pin debe ser mayor a 0000");
+                            return;
+                        }
+                        
+                    }
+                }
+                Console.WriteLine("Cuenta no encontrada.");
+            }
+            else
+            {
+                Console.WriteLine("El archivo de usuarios no existe.");
+            }
 
+        }
     }
-
-   
     
 }
--- Cajero/Program.cs
+++ Cajero/Program.cs
@@ -14,35 +14,29 @@ namespace Cajero
         static void Main(string[] args)
         {
 
-            // Instancia de la clase Usuario
+            // Ruta del archivo de texto que contiene los usuarios
             string rutaUsuarios = ("C:\\Users\\Usuario\\Documents\\Git\\CajeroPrueba\\Archivos txt\\Usuarios.txt");
-            string rutaMovimientos = ("C:\\Users\\Usuario\\Documents\\Git\\CajeroPrueba\\Archivos txt\\Movimientos txt");
-           // Usuario usuario = new Usuario();
+            
+            // Solicitar al usuario su nombre de usuario y PIN
 
-            //List<Usuario> ListaUsurios = CargarUsuarios(rutaUsuarios);
-            
-            //defino la ruta del archivo de texto donde se almacenan los usuarios y movimientos
- 
-                 
-            
             Console.WriteLine("Digite su usuario");
             string ComprobarUsuario = Console.ReadLine().Trim();
             Console.WriteLine();
             Console.WriteLine("Digite su pin");
             string ComprobarPin = Console.ReadLine().Trim();
 
-            Usuario accesoConcedido = iSA (rutaUsuarios, ComprobarUsuario,  ComprobarPin);
+            Usuario accesoConcedido = Usuario.iSA(rutaUsuarios, ComprobarUsuario, ComprobarPin); 
+            // Se llama al metodo iSA para validar el usuario y el pin
 
 
-            if (accesoConcedido != null)
-       
+            if (accesoConcedido != null)// Si el usuario es valido se le concede acceso
+
             {
-                //List<Usuario> listaUsuarios = CargarUsuarios(rutaUsuarios);
 
                 bool controlador = true;
-                while (controlador)
+                while (controlador) // se crea un ciclo para mostrar el menu hasta que el usuario decida salir
                 {
-                    Console.Clear();
+                   
                     Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
                     Console.WriteLine(":: Bienvenido al cajero automatico ::");
                     Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
@@ -52,55 +46,78 @@ namespace Cajero
                     Console.WriteLine(":: 2. Retirar                      ::");
                     Console.WriteLine(":: 3. Consultar saldo              ::");
                     Console.WriteLine(":: 4. Movimientos                  ::");
-                    Console.WriteLine(":: 5. Salir                        ::");
+                    Console.WriteLine(":: 5. Cambiar clave                ::");
+                    Console.WriteLine(":: 6. Salir                        ::");
                     Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
                     Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
 
 
                     int opcion = int.Parse(Console.ReadLine());
 
-                    switch (opcion)
+                    switch (opcion) // se crea un switch para seleccionar la opcion del menu
                     {
 
                         case 1:
-                            accesoConcedido.Depositar();
+                            accesoConcedido.Depositar(); // se llama al metodo depositar
+                            Console.WriteLine();
+                            Console.WriteLine("Oprima cualquier tecla para volver al menu");
+                            Console.ReadKey(true);
+                            Console.Clear();
                             break;
 
                         case 2:
-                            accesoConcedido.Retirar();
+                            accesoConcedido.Retirar(); // se llama al metodo retirar
+                            Console.WriteLine();
+                            Console.WriteLine("Oprima cualquier tecla para volver al menu");
+                            Console.ReadKey(true);
+                            Console.Clear();
                             break;
                         case 3:
-                            accesoConcedido.VerSaldo();
+                            accesoConcedido.VerSaldo(); // se llama al metodo ver saldo
+                            Console.WriteLine();
+                            Console.WriteLine("Oprima cualquier tecla para volver al menu");
+                            Console.ReadKey(true);
+                            Console.Clear();
                             break;
                         case 4:
-                            List<string> usuarios = Usuario.MostrarMovimiento(  accesoConcedido.NumeroCuenta);
+                           accesoConcedido.MostrarMovimiento(accesoConcedido.NumeroCuenta); // se llama al metodo mostrar movimientos
+                            Console.WriteLine("Oprima cualquier tecla para volver al menu");
+                            Console.ReadKey(true);
+                            Console.Clear();
                             break;
-
                         case 5:
-                            controlador = false;
+                            accesoConcedido.CambiarPin(); // se llama al metodo cambiar pin
+                            Console.WriteLine();
+                            Console.WriteLine("Oprima cualquier tecla para volver al menu");
+                            Console.ReadKey(true);
+                            Console.Clear();
+
                             break;
 
-
-
+                        case 6:
+                            controlador = false; // se cambia el valor del controlador para salir del ciclo
+                            Console.WriteLine("Gracias por usar el cajero automatico");
+                            Console.ReadKey(true); // se espera a que el usuario oprima una tecla
+                            Console.Clear();
+                            break;
 
                     }
+                    
                 }
-              
+
             }
             else
             {
-                Console.Clear();
                 Console.WriteLine("Acceso denegado. Usuario o PIN incorrecto.");
-                accesoConcedido = null; // Salir del programa si el acceso es denegado
+                //Acceso es denegado
             }
 
+            
             Console.ReadKey(true);
 
         }
-        
-      
-        
-        }
 
-            
+    }
+
+
 }
